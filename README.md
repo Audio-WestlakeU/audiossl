242eec5b8329911547b615c79115a46bc1ce60bc
lixian <shysian@gmail.com>
2022-03-31 05:36:46 +0000

    first  commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Self Supervised Learning for Audio Signal
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup,find_packages
+
+
+packages=find_packages(where=".")
+
+setup(
+    name='audiossl',
+    version='0.1',
+    description='',
+    author='lixian',
+    packages=packages,
+)
