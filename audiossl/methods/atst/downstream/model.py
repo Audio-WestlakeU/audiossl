719b98a62e1125355bb2e43a916f9f9e735a236a
lixian <shysian@gmail.com>
2022-04-08 09:43:53 +0000

    finetune work in progress

--- audiossl/methods/atst/downstream/model.py
+++ audiossl/methods/atst/downstream/model.py
@@ -6,6 +6,8 @@ import torch
 from torch import nn
 from torch.nn import functional as F
 from audiossl.methods.atst.downstream.utils import Metric
+from audiossl.utils.common import cosine_scheduler_epoch,get_params_groups
+from itertools import chain
 
 
 def binary_cross_entropy_with_logits(x: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
@@ -20,14 +22,13 @@ class PretrainedEncoderPLModule(LightningModule):
                  n_blocks: int):
         super().__init__()
         self.encoder = pretrained_encoder
-        self.encoder.eval()
         self.chunk_len = int((chunk_len * 16000)/160 + 1)
         self.n_blocks = n_blocks
+        self.embed_dim = self.encoder.embed_dim*2*n_blocks
 
     def forward(self, batch):
         (x, length), y = batch
-        with torch.no_grad():
-            x = self.encoder.get_intermediate_layers_chunks(x,
+        x = self.encoder.get_intermediate_layers_chunks(x,
                                                         length,
                                                         self.n_blocks,
                                                         self.chunk_len)
@@ -114,8 +115,109 @@ class LinearClassifierPLModule(LightningModule):
     @staticmethod
     def add_model_specific_args(parent_parser):
 
-        parser = parent_parser.add_argument_group("FreezedTransfer")
+        parser = parent_parser.add_argument_group("LinearClassifierModel")
         parser.add_argument("--learning_rate", default=0.002,
                             type=float, help="""Learning rate""")
         parser.add_argument('--max_epochs', default=100, type=int)
+        return parent_parser
+
+class FineTuningPLModule(LightningModule):
+    def __init__(self,
+                 encoder,
+                 learning_rate,
+                 max_epochs,
+                 niter_per_epoch,
+                 warmup_epochs,
+                 num_labels,
+                 multi_label=False,
+                 **kwargs):
+        super().__init__()
+        self.learning_rate = learning_rate
+        self.max_epochs = max_epochs
+        self.warumup_epochs = warmup_epochs
+        self.niter_per_epoch = niter_per_epoch
+
+        self.encoder = encoder
+        self.head = LinearHead(encoder.embed_dim, num_labels)
+        self.multi_label = multi_label
+
+        if multi_label:
+            self.loss_fn = binary_cross_entropy_with_logits
+            self.metric = Metric(mode="mAP")
+        else:
+            self.loss_fn = nn.CrossEntropyLoss()
+            self.metric = Metric(mode="ACC")
+        self.mylr_scheduler = cosine_scheduler_epoch(learning_rate,
+                                                     1e-6,
+                                                     max_epochs,
+                                                     niter_per_epoch,
+                                                     warmup_epochs)
+        self.save_hyperparameters(ignore=["encoder"])
+
+    def training_step(self, batch, batch_idx):
+        self.schedule()
+        x,y=self.encoder(batch)
+        if self.multi_label == False and y.dim() > 1:
+            y = y.argmax(-1)
+        x = self.head(x)
+        loss = self.loss_fn(x,y)
+        self.log("lr",self.trainer.optimizers[0].param_groups[0]["lr"],prog_bar=True,logger=True)
+        self.optimizers()
+        return loss
+
+    def schedule(self):
+        for i, param_group in enumerate(self.trainer.optimizers[0].param_groups):
+            param_group["lr"] = self.mylr_scheduler[self.global_step]
+        self.log("lr",param_group["lr"],prog_bar=True,logger=True)
+
+    def _cal_metric(self,output,target):
+        if self.multi_label:
+            self.metric.update(output.sigmoid().cpu().numpy(),target.cpu().numpy())
+        else:
+            self.metric.update(output.cpu(),target.cpu())
+
+
+    def validation_step(self, batch, batch_idx):
+        x,y=self.encoder(batch)
+        if self.multi_label == False and y.dim() > 1:
+            y = y.argmax(-1)
+        x = self.head(x)
+        loss = self.loss_fn(x,y)
+        self._cal_metric(x,y)
+        return loss
+    def on_validation_epoch_end(self) -> None:
+        metric  = self.metric.compute()
+        self.metric.clear()
+        self.log("val_"+self.metric.mode,metric,prog_bar=True,logger=True)
+
+
+    def test_step(self, batch, batch_idx):
+        x,y=self.encoder(batch)
+        if self.multi_label == False and y.dim() > 1:
+            y = y.argmax(-1)
+        x = self.head(x)
+        loss = self.loss_fn(x,y)
+        self._cal_metric(x,y)
+        return loss
+    def on_test_epoch_end(self) -> None:
+        metric  = self.metric.compute()
+        self.metric.clear()
+        self.log("test_"+self.metric.mode,metric,prog_bar=True,logger=True)
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.SGD(list(self.encoder.encoder.parameters())+list(self.head.parameters()),
+                                    self.learning_rate,
+                                    momentum=0.9,
+                                    weight_decay=0,
+                                    )
+        return [optimizer]
+
+    @staticmethod
+    def add_model_specific_args(parent_parser):
+
+        parser = parent_parser.add_argument_group("FineTuningModel")
+        parser.add_argument("--learning_rate", default=0.002,
+                            type=float, help="""Learning rate""")
+        parser.add_argument('--max_epochs', default=100, type=int)
+        parser.add_argument('--warmup_epochs', default=5, type=int)
         return parent_parser
