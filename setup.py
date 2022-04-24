c9ad943c36152ebe4d325e899878bc2867a693f5
lixian <shysian@gmail.com>
2022-04-24 06:58:36 +0000

    update

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setup(
     packages=packages,
     entry_points={
                   'console_scripts':[
+                                    'atst_train=audiossl.methods.atst.train:main' ,
                                     'atst_downstream_train_freeze=audiossl.methods.atst.downstream.train_freeze:main' ,
                                     'atst_downstream_train_finetune=audiossl.methods.atst.downstream.train_finetune:main' 
                                     ]
