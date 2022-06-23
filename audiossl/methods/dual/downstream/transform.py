47714cc509be3ad1c217586f14411e5915f17d24
lixian <shysian@gmail.com>
2022-06-23 17:31:47 +0000

    update

--- audiossl/methods/dual/downstream/transform.py
+++ audiossl/methods/dual/downstream/transform.py
@@ -6,7 +6,7 @@ from audiossl.transforms.target_transform import MixupSpecLabel
 from torchvision import transforms
 
 class FreezingTransform:
-    def __init__(self,sr=16000,max_len=12):
+    def __init__(self,sr=16000,max_len=9.5):
         melspec_t = torchaudio.transforms.MelSpectrogram(
             sr, f_min=60, f_max=7800, hop_length=160, win_length=1024, n_fft=1024, n_mels=64)
         to_db = torchaudio.transforms.AmplitudeToDB(stype="power",top_db=80)
@@ -31,7 +31,7 @@ class FreezingTransform:
         return output,output.shape[-1]
 
 class FinetuneTrainTransform:
-    def __init__(self,sr=16000,max_len=12):
+    def __init__(self,sr=16000,max_len=9.5):
         melspec_t = torchaudio.transforms.MelSpectrogram(
             sr, f_min=60, f_max=7800, hop_length=160, win_length=1024, n_fft=1024, n_mels=64)
         to_db = torchaudio.transforms.AmplitudeToDB(stype="power",top_db=80)
@@ -56,7 +56,7 @@ class FinetuneTrainTransform:
         return output,output.shape[-1]
 
 class FinetuneEvalTransform:
-    def __init__(self,sr=16000,max_len=12):
+    def __init__(self,sr=16000,max_len=9.5):
         melspec_t = torchaudio.transforms.MelSpectrogram(
             sr, f_min=60, f_max=7800, hop_length=160, win_length=1024, n_fft=1024, n_mels=64)
         to_db = torchaudio.transforms.AmplitudeToDB(stype="power",top_db=80)
