df006d2d105705f9adfbf83c540d29753ac529c2
lixian <shysian@gmail.com>
2023-04-03 16:53:01 +0800

    add dcase modules

---
+++ audiossl/datasets/dcase_utils/__init__.py
@@ -0,0 +1,4 @@
+from .datasets import WeakSet, StronglyAnnotatedSet, UnlabeledSet
+from .encoder import ManyHotEncoder
+from .sampler import ConcatDatasetSampler
+from .collate_fn import collate_fn
---
+++ audiossl/datasets/dcase_utils/collate_fn.py
@@ -0,0 +1,23 @@
+from pytorch_lightning import LightningDataModule
+from torch.utils import data
+import torch
+import torch.nn.functional as F
+
+
+def collate_fn(data):
+    spec_l = []
+    length_l = []
+    label_l = []
+    filename_1 = []
+    for d in data:
+        spec_l.append(d[0][0])
+        length_l.append(d[0][1])
+        label_l.append(d[1])
+        filename_1.append(d[2])
+
+    max_len = max(length_l)
+    for i in range(len(spec_l)):
+        spec_l[i]=F.pad(spec_l[i],(0,max_len-length_l[i]))
+        length_l[i]=torch.tensor(length_l[i]).clone().detach()
+        label_l[i] = torch.tensor(label_l[i]).clone().detach()
+    return (torch.stack(spec_l),torch.stack(length_l)),torch.stack(label_l), filename_1
