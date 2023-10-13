983295dfd181bc6c31a164c9d939fc6a6bbb3449
lmaxwell <shysian@gmail.com>
2023-10-13 16:03:16 +0800

    layerwise lr schedule

--- audiossl/methods/atst/downstream/model.py
+++ audiossl/methods/atst/downstream/model.py
@@ -75,9 +75,9 @@ class LinearClassifierPLModule(LightningModule):
 
     def _cal_metric(self,output,target):
         if self.multi_label:
-            self.metric.update(output.sigmoid().cpu().numpy(),target.cpu().numpy())
+            self.metric.update(output.sigmoid(),target)
         else:
-            self.metric.update(output.cpu(),target.cpu())
+            self.metric.update(output,target)
 
 
     def validation_step(self, batch, batch_idx):
@@ -126,6 +126,49 @@ class LinearClassifierPLModule(LightningModule):
         parser.add_argument('--max_epochs', default=100, type=int)
         return parent_parser
 
+def layer_wise_lr_groups(model):
+
+    layer_decay = 0.75
+    num_layers = 12
+    lr_scales = list(layer_decay ** (num_layers - i) for i in range(num_layers + 1))
+    model.unfreeze()
+
+    groups = []
+    for name,param in model.named_parameters():
+        # encoder.mask_embed encoder.pos_embed encoder.patch_embed
+        # encoder.blocks.
+        # encoder.norm_frame
+        # head
+        name = name.replace("encoder.encoder","encoder") 
+        if not param.requires_grad:
+            continue
+
+        if name.startswith("encoder.mask_embed") or name.startswith("encoder.pos_embed") or name.startswith("encoder.patch_embed") :
+            groups.append({'params': param,
+                        'lr_scale' : lr_scales[0],})
+                        #'name':name})
+        elif name.startswith("encoder.cls_token"):
+            groups.append({'params': param,
+                        'lr_scale' : lr_scales[0],})
+
+        elif name.startswith("encoder.blocks"):
+            index = int(name.split(".")[2])
+            groups.append({'params': param,
+                        'lr_scale' : lr_scales[index],})
+                        #'name':name})
+        elif name.startswith("encoder.norm"):
+            groups.append({'params': param,
+                        'lr_scale' : lr_scales[-2],})
+                        #'name':name})
+        elif name.startswith("head"):
+            groups.append({'params': param,
+                        'lr_scale' : lr_scales[-1],})
+                        #'name':name})
+        else:
+            print("missed",name)
+    return groups
+
+
 class FineTuningPLModule(LightningModule):
     def __init__(self,
                  encoder,
@@ -135,6 +178,7 @@ class FineTuningPLModule(LightningModule):
                  warmup_epochs,
                  num_labels,
                  multi_label=False,
+                 layer_wise_lr=False,
                  mixup_training=False,
                  **kwargs):
         super().__init__()
@@ -147,6 +191,7 @@ class FineTuningPLModule(LightningModule):
         self.head = LinearHead(encoder.embed_dim, num_labels,use_norm=True, affine=False)
         self.multi_label = multi_label
         self.mixup_training = mixup_training
+        self.layer_wise_lr = layer_wise_lr
         self.num_labels = num_labels
 
         if multi_label or self.mixup_training:
@@ -180,9 +225,14 @@ class FineTuningPLModule(LightningModule):
         return loss
 
     def schedule(self):
-        for i, param_group in enumerate(self.trainer.optimizers[0].param_groups):
-            param_group["lr"] = self.mylr_scheduler[self.global_step]
-        self.log("lr",param_group["lr"],prog_bar=True,logger=True)
+
+        if self.layer_wise_lr:
+            for i, param_group in enumerate(self.trainer.optimizers[0].param_groups):
+                param_group["lr"] = self.mylr_scheduler[self.global_step] * param_group["lr_scale"]
+        else:
+            for i, param_group in enumerate(self.trainer.optimizers[0].param_groups):
+                param_group["lr"] = self.mylr_scheduler[self.global_step]
+        self.log("lr",self.mylr_scheduler[self.global_step],prog_bar=True,logger=True)
 
     def _cal_metric(self,output,target):
         if self.multi_label:
@@ -228,11 +278,18 @@ class FineTuningPLModule(LightningModule):
         self.log("test_"+self.metric.mode,metric,prog_bar=True,logger=True)
 
     def configure_optimizers(self):
-        optimizer = torch.optim.SGD(list(self.encoder.encoder.parameters())+list(self.head.parameters()),
-                                    self.learning_rate,
-                                    momentum=0.9,
-                                    weight_decay=0,
-                                    )
+        if self.layer_wise_lr:
+            optimizer = torch.optim.SGD(layer_wise_lr_groups( self),
+                                        self.learning_rate,
+                                        momentum=0.9,
+                                        weight_decay=0,
+                                        )
+        else:
+            optimizer = torch.optim.SGD(list(self.encoder.encoder.parameters())+list(self.head.parameters()),
+                                self.learning_rate,
+                                momentum=0.9,
+                                weight_decay=0,
+                                )
         return [optimizer]
 
     @staticmethod
