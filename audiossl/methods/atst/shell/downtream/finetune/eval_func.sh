134b65015bfd9b6fce34b28bb75444e7b083b8df
lmaxwell <shysian@gmail.com>
2023-10-19 14:58:00 +0800

    fix

--- audiossl/methods/atst/shell/downtream/finetune/eval_func.sh
+++ audiossl/methods/atst/shell/downtream/finetune/eval_func.sh
@@ -19,12 +19,12 @@ eval()
         local save_path=`dirname $pretrained_ckpt_path`/${ds_name}/last_blocks_${n_last_blocks}_batchsize${batch_size}_lr${lr}_mixup_${mixup_training}_max_epochs${max_epochs}_rrc${rrc}_layerwiselr${layerwise_lr}_maskaug${mask_aug}_alpha${alpha}
 
 
-        
+
         local log=$save_path/verbose.txt
-        
 
 
-        #cmd_str="CUDA_VISIBLE_DEVICES=$DEVICE $cmd --n_last_blocks $n_last_blocks 
+
+        #cmd_str="CUDA_VISIBLE_DEVICES=$DEVICE $cmd --n_last_blocks $n_last_blocks
         #              --batch_size_per_gpu $batch_size
         #              --dataset_name $ds_name
         #              --data_path $data_path
@@ -33,11 +33,11 @@ eval()
         #              --learning_rate $lr
         #              --save_path $save_path"
 
-        if [ $DEBUG -eq 1 ] 
-        then 
-            echo =============model===============: 
+        if [ $DEBUG -eq 1 ]
+        then
+            echo =============model===============:
             echo $pretraind_ckpt_path
-            echo =============output_dir==========: 
+            echo =============output_dir==========:
             echo $save_path
             echo ===============log===============:
             echo $log
@@ -51,17 +51,15 @@ eval()
                       --max_epochs $max_epochs\
                       --warmup_epochs $warmup_epochs\
                       --mixup_training $mixup_training\
-
 		      --rrc $rrc\
 		      --layerwise_lr $layerwise_lr\
 		      --mask_aug $mask_aug\
 		      --alpha $alpha\
-
                       --nproc $NPROC\
-                      --save_path $save_path 
-	    
+                      --save_path $save_path
+
             exit
-    
+
         fi
 
         if [ ! -s $pretraind_ckpt_path ];then
@@ -74,7 +72,7 @@ eval()
                 exit
         fi
         mkdir -p $save_path
-        
+
 
         CUDA_VISIBLE_DEVICES=$DEVICE $cmd --n_last_blocks $n_last_blocks \
                       --batch_size_per_gpu $batch_size\
@@ -85,12 +83,10 @@ eval()
                       --max_epochs $max_epochs\
                       --warmup_epochs $warmup_epochs\
                       --mixup_training $mixup_training\
-
 		      --rrc $rrc\
 		      --layerwise_lr $layerwise_lr\
 		      --mask_aug $mask_aug\
 		      --alpha $alpha\
-
                       --nproc $NPROC\
                       --save_path $save_path > $log 2>&1
 }
